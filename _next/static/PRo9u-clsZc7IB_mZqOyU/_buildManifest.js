d2fffa7273766c7dabc9b5be969d87689434e784
digiz3d <1338620+digiz3d@users.noreply.github.com>
2021-05-08 18:53:55 +0000

    Deploying to gh-pages from @ digiz3d/digiz3d.github.io@054abb8de91ca44cf17f65b0e94d9723f62d732d 🚀

--- _next/static/PRo9u-clsZc7IB_mZqOyU/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/83cb9b65d75a71c7b070.css","static/chunks/pages/index-ad5b9fed7460d8134b4f.js"],"/_error":["static/chunks/pages/_error-0add9c45beeae6bfd606.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
