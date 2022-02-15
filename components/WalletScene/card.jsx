a21347795be736d83d94fd3be4e81e72312eb61f
Leandre Daumont <1338620+digiz3d@users.noreply.github.com>
2022-02-15 18:03:37 +0100

    rework card

--- components/WalletScene/card.jsx
+++ components/WalletScene/card.jsx
@@ -6,16 +6,9 @@ export default function Card(props) {
   const { nodes, materials } = useGLTF('/card.glb')
   return (
     <group ref={group} {...props} dispose={null}>
-      <group rotation={[Math.PI / 2, 0, 0]} scale={[0.0865, 0.00068, 0.05398]}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Cube_1.geometry}
-          material={materials.front}
-        />
-        <mesh castShadow receiveShadow geometry={nodes.Cube_2.geometry} material={materials.side} />
-        <mesh castShadow receiveShadow geometry={nodes.Cube_3.geometry} material={materials.back} />
-      </group>
+      <mesh castShadow receiveShadow geometry={nodes.Card_1.geometry} material={materials.front} />
+      <mesh castShadow receiveShadow geometry={nodes.Card_2.geometry} material={materials.side} />
+      <mesh castShadow receiveShadow geometry={nodes.Card_3.geometry} material={materials.back} />
     </group>
   )
 }
--- components/WalletScene/index.jsx
+++ components/WalletScene/index.jsx
@@ -1,12 +1,15 @@
 import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
+import { Suspense } from 'react'
 import Card from './card'
 
 export default function WalletScene() {
   return (
     <Canvas camera={{ position: [0, 0, 1], fov: 40, near: 0.0001, far: 10 }}>
       <ambientLight intensity={1} />
-      <Card />
+      <Suspense fallback={null}>
+        <Card />
+      </Suspense>
       <OrbitControls />
     </Canvas>
   )
