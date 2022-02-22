176733e33030fa99ddc3990c18db6f1d1feadcf9
phoz0n <phozon@protonmail.com>
2022-02-22 15:06:01 +0100

    fixed slower animation

---
+++ components/WalletScene/Poop2.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef, useState } from 'react'
+import { useGLTF } from '@react-three/drei'
+import { motion } from 'framer-motion-3d'
+
+export default function Poop(props) {
+  const [hover, set] = useState(false)
+
+  useEffect(() => {
+    document.body.style.cursor = hover ? 'pointer' : 'default'
+  }, [hover])
+
+  const group = useRef()
+  const { nodes, materials } = useGLTF('/poop.glb')
+  return (
+    <motion.group ref={group} {...props} dispose={null}>
+      <motion.mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Poop.geometry}
+        material={materials.Material}>
+      </motion.mesh>
+    </motion.group>
+  )
+}
+
+useGLTF.preload('/poop.glb')
--- components/WalletScene/index.jsx
+++ components/WalletScene/index.jsx
@@ -4,6 +4,7 @@ import * as THREE from 'three'
 import { Canvas } from '@react-three/fiber'
 
 import Poop from './Poop'
+import Poop2 from './Poop2'
 import { OrbitControls } from '@react-three/drei'
 
 export default function WalletScene() {
@@ -19,10 +20,11 @@ export default function WalletScene() {
         <motion.pointLight position={[2, 2, -2]} intensity={2} />
         <Poop
           rotation={[Math.PI / 2, 0, 0]}
-          transition={{ duration: 0.5 }}
-          initial={{ scale: 0, rotateX: Math.PI * 2 + 0.5 }}
-          animate={{ scale: 0.1, rotateZ: Math.PI * 2 }}
+          transition={{ duration: 3 }}
+          initial={{ scale: 0, rotateX: Math.PI * 2 + 0.2 }}
+          animate={{ scale: 0.1, rotateY: Math.PI * 2 + 0.5 }}
         />
+
       </Suspense>
     </Canvas>
   )
